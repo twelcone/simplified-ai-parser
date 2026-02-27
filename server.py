5abff1e8e6220f8a223b46b81c2fd31759e9fc0b
twelcone <vuquocminhdang@gmail.com>
2026-02-27 18:12:35 +0400

    Fix: docker

--- server.py
+++ server.py
@@ -10,6 +10,6 @@ def is_port_in_use(port: int) -> bool:
 if __name__ == "__main__":
     port = 7656
     if not is_port_in_use(port):
-        uvicorn.run("app.main:app", host="localhost", port=port, reload=True)
+        uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
     else:
         print(f"Port {port} is already in use. Please free up the port and try again.")
